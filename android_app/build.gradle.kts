b02d0ab761e800d6cb7b4232b7d411ca33c2cf8a
Krishna Reddy sajjala <krishnareddysajjala0@gmail.com>
2026-09-18 18:24:49 +0530

    Dedicated iOS IPA project: remove Android specific files

--- android_app/build.gradle.kts
+++
@@ -1,5 +0,0 @@
-plugins {
-    id("com.android.application") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    id("com.chaquo.python") version "16.1.0" apply false
-}
