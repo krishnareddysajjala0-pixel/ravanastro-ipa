f74a08e087aea96c02edf44d1c3489af39891930
Krishna Reddy sajjala <krishnareddysajjala0@gmail.com>
2026-09-18 19:24:39 +0530

    Configure App target build settings via configure_app.rb with enhanced error logging

---
+++ ios_src/configure_app.rb
@@ -0,0 +1,22 @@
+require 'xcodeproj'
+
+project_path = 'App.xcodeproj'
+project = Xcodeproj::Project.open(project_path)
+target = project.targets.find { |t| t.name == 'App' } || project.targets.first
+
+target.build_configurations.each do |config|
+  config.build_settings['SWIFT_OBJC_BRIDGING_HEADER'] = 'App/App-Bridging-Header.h'
+  config.build_settings['HEADER_SEARCH_PATHS'] ||= ['$(inherited)']
+  config.build_settings['HEADER_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks/Python.xcframework/ios-arm64/Python.framework/Headers'
+  
+  config.build_settings['FRAMEWORK_SEARCH_PATHS'] ||= ['$(inherited)']
+  config.build_settings['FRAMEWORK_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks/ios-arm64'
+  config.build_settings['FRAMEWORK_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks'
+  
+  config.build_settings['OTHER_LDFLAGS'] ||= ['$(inherited)']
+  config.build_settings['OTHER_LDFLAGS'] << '-framework Python'
+  config.build_settings['ENABLE_BITCODE'] = 'NO'
+end
+
+project.save
+puts 'Successfully configured App target build settings in App.xcodeproj!'
