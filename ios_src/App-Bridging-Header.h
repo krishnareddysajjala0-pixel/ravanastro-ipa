6ee7c8966c2825e385e03083b014d9f4b8b75d92
Krishna Reddy sajjala <krishnareddysajjala0@gmail.com>
2026-09-18 19:20:32 +0530

    Configure direct xcodebuild flags and bundle embedding for offline Python engine

--- ios_src/App-Bridging-Header.h
+++ ios_src/App-Bridging-Header.h
@@ -1,6 +1,10 @@
 #ifndef App_Bridging_Header_h
 #define App_Bridging_Header_h
 
+#if __has_include(<Python.h>)
+#include <Python.h>
+#elif __has_include(<Python/Python.h>)
 #include <Python/Python.h>
+#endif
 
 #endif /* App_Bridging_Header_h */
