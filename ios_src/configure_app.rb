55fe4106368583a2496bfa3e68ea6951991b7ce6
Krishna Reddy sajjala <krishnareddysajjala0@gmail.com>
2026-09-18 19:28:55 +0530

    Update configure_app.rb paths and add build log artifact upload

--- ios_src/configure_app.rb
+++ ios_src/configure_app.rb
@@ -8,13 +8,14 @@ target.build_configurations.each do |config|
   config.build_settings['SWIFT_OBJC_BRIDGING_HEADER'] = 'App/App-Bridging-Header.h'
   config.build_settings['HEADER_SEARCH_PATHS'] ||= ['$(inherited)']
   config.build_settings['HEADER_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks/Python.xcframework/ios-arm64/Python.framework/Headers'
+  config.build_settings['HEADER_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks/Python.xcframework/ios-arm64/include/python3.11'
   
   config.build_settings['FRAMEWORK_SEARCH_PATHS'] ||= ['$(inherited)']
-  config.build_settings['FRAMEWORK_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks/ios-arm64'
+  config.build_settings['FRAMEWORK_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks/Python.xcframework/ios-arm64'
   config.build_settings['FRAMEWORK_SEARCH_PATHS'] << '$(PROJECT_DIR)/Frameworks'
   
   config.build_settings['OTHER_LDFLAGS'] ||= ['$(inherited)']
-  config.build_settings['OTHER_LDFLAGS'] << '-framework Python'
+  config.build_settings['OTHER_LDFLAGS'] << '-F$(PROJECT_DIR)/Frameworks/Python.xcframework/ios-arm64 -framework Python'
   config.build_settings['ENABLE_BITCODE'] = 'NO'
 end
 
